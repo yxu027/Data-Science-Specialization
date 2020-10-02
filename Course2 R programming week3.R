53c2dd085ff3284171e6f9bc7dc8f025fb412421
yxu027 <yxu027@ucr.edu>
2020-10-01 19:41:06 -0700

    final version

--- Course2 R programming week3.R
+++ Course2 R programming week3.R
@@ -25,40 +25,37 @@ best <- function(state, outcome) {
 
 #3. Ranking hospitals by outcome in a state
 rankhospital <- function(state, outcome, num = "best") {
-    ## Read outcome data
-    data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
-    ## Check that state and outcome are valid
-    if (!state %in% data[,7]) {
-        stop("invalid state")
+  ## Read outcome data
+  data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+  ## Check that state and outcome are valid
+  if (!state %in% data[,7]) {
+    stop("invalid state")
+  }
+  else if (!outcome %in% c("heart attack", "heart failure", "pneumonia")) {
+    stop("invalid outcome")
+  }
+  else {
+    col <- c("heart attack" = 11, "heart failure" = 17, "pneumonia" = 23)
+    data[, col[outcome]] <- as.numeric(data[, col[outcome]])
+    splitstate <- split(data, data[,7])
+    certainstate <- splitstate[[state]]
+    oderedstate <- certainstate[order(certainstate[, col[outcome]], certainstate[,2]),]
+    naremove <- oderedstate[!is.na(oderedstate[, col[outcome]]),]
+    ## Return hospital name in that state with the given rank 30-day death rate
+    if (num == "best"){
+      naremove[1 ,2]
     }
-    else if (!outcome %in% c("heart attack", "heart failure", "pneumonia")) {
-      stop("invalid outcome")
+    else if (num == "worst"){
+      naremove[nrow(naremove),2]
+    }
+    else if (as.numeric(num) > nrow(naremove)){
+      NA
     }
     else {
-        col <- c("heart attack" = 11, "heart failure" = 17, "pneumonia" = 23)
-        data[, col[outcome]] <- as.numeric(data[, col[outcome]])
-        splitstate <- split(data, data[,7])
-        certainstate <- splitstate[[state]]
-        oderedstate <- lapply(splitstate, function(x) x[order(x[, 11], x[, 2]),])
-        naremove <- lapply(oderedstate, function(x) x[!is.na(x[, col[outcome]]),]
-                           
-                        
-        ## Return hospital name in that state with the given rank 30-day death rate
-        if (num == "best"){
-            naremove[1 ,2]
-        }
-        else if (num == "worst"){
-            naremove[nrow(naremove),2]
-        }
-        else if (as.numeric(num) > nrow(naremove)){
-            NA
-        }
-        else {
-            naremove[as.numeric(num) ,2]
-        }
+      naremove[as.numeric(num) ,2]
     }
+  }
 }
-
 
 #4. Ranking hospitals in all states
 rankall <- function(outcome, num = "best") {
